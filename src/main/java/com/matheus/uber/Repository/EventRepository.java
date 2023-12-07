af28835783dd35c11a68c8cf9e56c72014a91b35
Mathe <matheusamrl@gmail.com>
2023-12-07 20:19:41 -0300

    Repositories e Subscription

---
+++ src/main/java/com/matheus/uber/Repository/EventRepository.java
@@ -0,0 +1,30 @@
+package com.matheus.uber.Repository;
+
+
+import com.matheus.uber.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, String> {
+
+    @Query(value = "SELECT * FROM events e WHERE parsedatetime(e.date, 'dd/MM/yyyy') > :currentDate", nativeQuery = true)
+    List<Event> findUpcomingEvents(@Param("currentDate") LocalDateTime currentDate);
+    /* Aqui estamos buscando eventos que irão acontecer no futuro, em que a data ainda não passou
+    Com essa anotação ele checa a data que estamos antes de trazer o evento na lista */
+
+    @NonNull
+    Optional<Event> findById(@NonNull String id);
+    /* Aqui usamos a geração com UUID, mas como estamos usando essa estrategia
+    o JPA pensa q estamos usando uuid(Java Util) mas queremos uma String,
+     por isso estamos declarando.
+     */
+
+}
---
+++ src/main/java/com/matheus/uber/model/Subscription.java
@@ -0,0 +1,25 @@
+package com.matheus.uber.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "subscription")
+@Entity(name = "subscription")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Event event;
+
+    private String emailPart;
+
+}
